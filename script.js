b7b06a18613aeb8fccbf7092f329815046166c5b
VidathD <dmvlayanga@gmail.com>
2026-09-16 15:27:16 +0530

    fix redirect not having

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ function startRedirect() {
 
     if (secondsLeft <= 0) {
       window.clearInterval(timer);
-      window.location.replace(destination);
+      window.location.assign(destination);
     }
   }, 1000);
 }
